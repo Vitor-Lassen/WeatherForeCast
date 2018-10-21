070c97d85b00667df75895cbc48b7ef2366ebe92
Vitor Lassen <vitor@lassen.com.br>
2018-10-21 20:14:56 -0300

    Final da aula

--- app/src/main/java/com/example/vitor/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/vitor/weatherforecast/MainActivity.java
@@ -8,8 +8,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText locationEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +31,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        locationEditText = findViewById(R.id.locationEditText);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String cidade = locationEditText.getEditableText().toString();
+                            String chave = getString(R.string.api_key);
+                            String end = getString(R.string.web_service_url,cidade,chave);
+                            URL url = new URL(end);
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            InputStream stream = connection.getInputStream();
+                            InputStreamReader inputStreamReader =new InputStreamReader(stream);
+
+                            StringBuilder sb = new StringBuilder("");
+                            //try with resources
+                            try (BufferedReader reader = new BufferedReader(inputStreamReader)){
+                                String linha = null;
+                                String json = "";
+
+                                while ((linha = reader.readLine()) != null){
+                                    sb.append( linha);
+                                }
+                                //bloco try() funciona semelhante ao using , ao sair do bloco as conexões são fechadas
+                                //reader.close();
+                            }
+
+                            connection.disconnect();
+                            runOnUiThread(()-> {
+                                Toast.makeText(MainActivity.this,sb.toString(),Toast.LENGTH_LONG).show();
+                            });
+                        }
+                        catch (IOException ex) {
+                            ex.printStackTrace();
+                            runOnUiThread(()-> {
+                                Toast.makeText(MainActivity.this,ex.toString(),Toast.LENGTH_LONG).show();
+                            });
+                        }
+                    }
+                }).start();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
